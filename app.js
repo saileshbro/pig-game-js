f070d447afefb54377636fa8c15bae89063559ad
Sailesh Dahal <saileshbro@gmail.com>
2018-10-16 11:34:57 +0545

    Game win condition created and first proto done.

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 var dice, currentScore, netScore, activePlayer;
-
+var maxValue;
 currentScore = 0;
 netScore = [0, 0];
 activePlayer = 0;
+
+var playerOne = prompt("Enter #1 name.");
+var playerTwo = prompt("Enter #2 name.");
+var maxValue = prompt("Enter game win value");
+
+document.getElementById('name-0').textContent = playerOne;
+document.getElementById('name-1').textContent = playerTwo;
+document.querySelector(".dice").src = 'dice-0.gif';
 document.querySelector('.btn-roll').addEventListener('click', function () {
     dice = Math.ceil(Math.random() * 6); //dice roll
     document.querySelector(".dice").src = 'dice-' + dice + '.png';
@@ -27,7 +35,7 @@ document.querySelector('.btn-roll').addEventListener('click', function () {
 document.querySelector('.btn-hold').addEventListener('click', function () {
     netScore[activePlayer] += currentScore;
     currentScore = 0;
-    if (netScore[activePlayer] < 20) {
+    if (netScore[activePlayer] < maxValue) {
         document.getElementById('score-' + activePlayer).textContent = netScore[activePlayer];
         document.getElementById('current-' + activePlayer).textContent = currentScore;
         document.querySelector('.player-0-panel').classList.toggle('active');
@@ -45,4 +53,19 @@ document.querySelector('.btn-hold').addEventListener('click', function () {
     } else {
         activePlayer = 0;
     }
+
 });
+document.querySelector(".btn-new").addEventListener('click', newGame);
+
+function newGame() {
+    currentScore = 0;
+    netScore = [0, 0];
+    activePlayer = 0;
+    document.querySelector("#current-0").textContent = 0;
+    document.querySelector("#current-1").textContent = 0;
+    document.querySelector("#score-0").textContent = 0;
+    document.querySelector("#score-1").textContent = 0;
+    document.querySelector(".dice").src = 'dice-0.gif';
+    document.querySelector('.player-0-panel').classList.add('active');
+    document.querySelector('.player-1-panel').classList.remove('active');
+}
