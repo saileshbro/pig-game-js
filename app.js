d633ae8cfc943b2ffe262c3378233cb0ba5afa56
Sailesh Dahal <saileshbro@gmail.com>
2018-10-16 09:31:15 +0545

    event for rolling dice created. condition for 1 on dice also added for rolling method

--- app.js
+++ app.js
@@ -6,6 +6,15 @@ document.querySelector('.btn-roll').addEventListener('click', function () {
     dice = Math.ceil(Math.random() * 6); //dice roll
     document.querySelector(".dice").src = 'dice-' + dice + '.png';
     if (dice != 1) {
-
+        currentScore += dice;
+        document.getElementById('current-' + activePlayer).textContent = currentScore;
+    }
+    if (dice === 1) {
+        currentScore = 0;
+        document.getElementById('current-' + activePlayer).textContent = currentScore;
+        activePlayer == 0 ? activePlayer = 1 : activePlayer = 0;
+        document.querySelector(".dice").style.display = 'none';
+        document.querySelector('.player-0-panel').classList.toggle('active');
+        document.querySelector('.player-1-panel').classList.toggle('active');
     }
 });
